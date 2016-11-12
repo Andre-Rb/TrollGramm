2fbdc1bcb1d30f03080fe2ad9a2401bfe6f305bd
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 08:58:16 +0100

    Changed player physics WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,8 @@
 public class Player : MonoBehaviour
 {
     public float speed = 8.0f;
-    public float height = 10.0f;
-    public float speedRotate = 100.0f;
+    public float JumpHeight = 10.0f;
+    public float rotationSpeed = 100.0f;
     Rigidbody rb;
     public bool Alive { get; set; }
 
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Move();
         Jump();
@@ -28,10 +28,11 @@ public class Player : MonoBehaviour
         {
 
             float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            float rotateH = Input.GetAxis("Horizontal") * speedRotate * Time.deltaTime;
+            float rotateH = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
 
-            transform.Translate(new Vector3(0, 0, moveV));
-
+            //transform.Translate(new Vector3(0, 0, moveV));
+            //GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * moveV);
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveV);
             transform.Rotate(new Vector3(0, rotateH, 0));
         }
     }
@@ -42,7 +43,7 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb.velocity = new Vector3(0, height, 0);
+                rb.velocity = new Vector3(0, JumpHeight, 0);
             }
         }
     }
