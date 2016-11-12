b8c733546b701639a7569a03017bfff8c06cc7aa
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 04:32:25 +0100

    added classes to prepare dialogue system

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue
+{
+
+    string lineText;
+    AudioClip lineclip;
+
+    public string LineText
+    {
+        get
+        {
+            return lineText;
+        }
+
+        set
+        {
+            lineText = value;
+        }
+    }
+
+    public AudioClip Lineclip
+    {
+        get
+        {
+            return lineclip;
+        }
+
+        set
+        {
+            lineclip = value;
+        }
+    }
+
+
+
+}
