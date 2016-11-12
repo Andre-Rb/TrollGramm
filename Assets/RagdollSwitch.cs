05a0306095e2d38d0d363069c8402b36b082f6ea
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 05:34:45 +0100

    Animations tests

---
+++ Assets/RagdollSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+
+
+public class RagdollSwitch : MonoBehaviour
+{
+    public GameObject bipedRootGameobject;
+
+    List
+    private List<CapsuleCollider> _colliders;
+    private List<CharacterJoint> _characterJoints;
+    // Use this for initialization
+    void Start()
+    {
+
+        _colliderList = bipedRootGameobject.GetComponentsInChildren<CapsuleCollider>();
+        _characterJoints = bipedRootGameobject.GetComponentsInChildren<CharacterJoint>();
+        Debug.Log("sqd");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
