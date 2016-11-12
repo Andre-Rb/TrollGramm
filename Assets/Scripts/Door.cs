075f66bb481e5331b37563cf2c7af787311d9f93
maldeirra <malde.clement@hotmail.fr>
2016-11-12 03:20:14 +0100

    Ajout des nouveaux Layers + disparition de la porte lorsque switch actif

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour 
+{
+	Switch bouton;
+
+	void Start ()
+	{
+		bouton = GameObject.FindObjectOfType<Switch> ();
+	}
+
+	void Update () 
+	{
+		
+		if(bouton.bouton == true)
+		{
+			gameObject.SetActive (false);
+		}
+	}
+}
