e5db741cad702f1b788139c56904592bc82572fe
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 22:28:05 +0100

    Removed import and added new start menu

---
+++ Assets/ButtonAction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonAction : MonoBehaviour
+{
+    private FadeGUI _fadeGui;
+
+
+    void Start()
+    {
+        _fadeGui = GetComponent<FadeGUI>();
+
+    }
+
+
+    public int firstScene;
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadSceneAsync(firstScene, LoadSceneMode.Single);
+        _fadeGui.gameObject.SetActive(true);
+        _fadeGui.BeginFade(1);
+
+
+    }
+
+}
