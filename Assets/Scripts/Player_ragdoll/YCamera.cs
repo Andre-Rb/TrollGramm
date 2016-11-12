8ada8f1ef782c3d2c1a9f11f210c5fa6e2a89706
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 09:02:12 +0100

    Merge

---
+++ Assets/Scripts/Player_ragdoll/YCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YCamera : MonoBehaviour 
+{
+	public float speedRotate = 200.0f;
+	public float max;
+	public float min;
+
+	void Update () 
+	{
+		Mouse ();
+	}
+
+	void Mouse()
+	{
+		float mouseY = -Input.GetAxis ("Mouse Y");
+		transform.rotation *= Quaternion.Euler (mouseY, 0,	0);
+	}
+}
---
+++ Assets/Scripts/SceneManager/NextScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour 
+{
+	void OnTriggerEnter()
+	{
+		SceneManager.LoadSceneAsync (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
