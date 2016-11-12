428a4d537eb483efc786bada5b4daa7f513693cc
maldeirra <malde.clement@hotmail.fr>
2016-11-12 05:20:55 +0100

    Ajout d'une petite musique + fin de la deuxième épreuve

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -4,13 +4,30 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour 
 {
+	static SceneManagement instance = null;
+
 	public string scene = "";
+	public bool openDoor = false;
+
+	void Start()
+	{
+		if(instance != null)
+		{
+			Destroy (gameObject);
+		}
+		else
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player") 
 		{
 			SceneManager.LoadScene (scene);
+			openDoor = true;
 		}
 	}
 }
