4bef490ac8491a1436c505ca9e9d41c7f0c6f6b4
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 06:28:19 +0100

    added killing with k key (editor only) the character.

--- Assets/RagdollSwitch.cs
+++ Assets/RagdollSwitch.cs
@@ -1,27 +1,76 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 
 
 public class RagdollSwitch : MonoBehaviour
 {
     public GameObject bipedRootGameobject;
+    public bool EnableRagdollColliders;
 
-    List
-    private List<CapsuleCollider> _colliders;
+
+    private List<Collider> _colliders;
     private List<CharacterJoint> _characterJoints;
-    // Use this for initialization
+
+
     void Start()
     {
+        _colliders = bipedRootGameobject.GetComponentsInChildren<Collider>().ToList();
+        _characterJoints = bipedRootGameobject.GetComponentsInChildren<CharacterJoint>().ToList();
+        Debug.Log(Application.isEditor);
 
-        _colliderList = bipedRootGameobject.GetComponentsInChildren<CapsuleCollider>();
-        _characterJoints = bipedRootGameobject.GetComponentsInChildren<CharacterJoint>();
-        Debug.Log("sqd");
+        SetAllRagDollColliders(EnableRagdollColliders);
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DebugPrintCollidersAndJoints()
+    {
+        Debug.Log("Found " + _colliders.Count.ToString() + " colliders and " + _characterJoints.Count.ToString() + " joints.");
+    }
+
+    private void SetAllRagDollColliders(bool b)
     {
 
+        foreach (Collider collider in _colliders)
+        {
+            collider.enabled = b;
+
+        }
+
+
+    }
+
+    public void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Kill();
+        }
+
+
+    }
+
+
+    /// <summary>
+    /// Permet de killer avec la touche K ( que dans l'editeur
+    /// </summary>
+    void Kill()
+    {
+        if (Application.isEditor)
+        {
+            SetAllRagDollColliders(true);
+            GetComponent<Animator>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+            Destroy(GetComponent<Rigidbody>());
+            Debug.Log("killed the character with kill method");
+            DebugPrintCollidersAndJoints();
+
+            //Debug.Break();
+
+        }
     }
 }
