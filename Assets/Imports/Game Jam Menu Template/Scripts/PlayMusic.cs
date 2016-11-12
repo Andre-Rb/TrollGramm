42ebcaef5fbdea97e28f92c2c24208571d301f43
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-12 21:01:53 +0100

    Imported Marc's menu and added new scene to test it

---
+++ Assets/Imports/Game Jam Menu Template/Scripts/PlayMusic.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class PlayMusic : MonoBehaviour {
+
+
+	public AudioClip titleMusic;					//Assign Audioclip for title music loop
+	public AudioClip mainMusic;						//Assign Audioclip for main 
+	public AudioClip level1music;
+
+
+
+	public AudioMixerSnapshot volumeDown;			//Reference to Audio mixer snapshot in which the master volume of main mixer is turned down
+	public AudioMixerSnapshot volumeUp;				//Reference to Audio mixer snapshot in which the master volume of main mixer is turned up
+
+
+	private AudioSource musicSource;				//Reference to the AudioSource which plays music
+	public float resetTime = .01f;					//Very short time used to fade in near instantly without a click
+
+
+
+
+	void Awake () 
+	{
+		//Get a component reference to the AudioSource attached to the UI game object
+		musicSource = GetComponent<AudioSource> ();
+		//Call the PlayLevelMusic function to start playing music
+	}
+
+
+	public void PlayLevelMusic()
+	{
+		//This switch looks at the last loadedLevel number using the scene index in build settings to decide which music clip to play.
+		switch (SceneManager.GetActiveScene().buildIndex)
+		{
+			//If scene index is 0 (usually title scene) assign the clip titleMusic to musicSource
+			case 0:
+				musicSource.clip = titleMusic;
+				break;
+			//If scene index is 1 (usually main scene) assign the clip mainMusic to musicSource
+			case 1:
+				musicSource.clip = mainMusic;
+				break;
+			case 2:
+				musicSource.clip = level1music;
+				break;
+		}
+		//Fade up the volume very quickly, over resetTime seconds (.01 by default)
+		FadeUp (.01f);
+		//Debug.Log ("test1, StartOption");
+		//Play the assigned music clip in musicSource
+		musicSource.Play ();
+	}
+
+
+	//Call this function to very quickly fade up the volume of master mixer
+	public void FadeUp(float fadeTime)
+	{
+
+		//call the TransitionTo function of the audioMixerSnapshot volumeUp;
+		volumeUp.TransitionTo (fadeTime);
+
+
+	}
+
+	//Call this function to fade the volume to silence over the length of fadeTime
+	public void FadeDown(float fadeTime)
+	{
+		
+
+		//call the TransitionTo function of the audioMixerSnapshot volumeDown;
+		volumeDown.TransitionTo (fadeTime);
+
+	}
+}
---
+++ Assets/Imports/Modif/Script/PreviousLevel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PreviousLevel : MonoBehaviour {
+	public int oldIndex;
+	public int nouvIndex;
+	public bool isOld;
+
+
+	// Use this for initialization
+	void OnLevelWasLoaded() {
+		
+		//Debug.Log ("test1, previouslevel");
+		nouvIndex = SceneManager.GetActiveScene().buildIndex;
+		isOld = (oldIndex == nouvIndex);
+		if (isOld) {
+			oldIndex = nouvIndex;
+			Debug.Log (nouvIndex + " " + oldIndex);
+
+
+		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
+//newscene.buildIndex = SceneManager.GetActiveScene.buildIndex ();
+// (SceneManager.GetActiveScene.buildIndex () == oldScene.buildIndex) {
+
+//private string Scene.name (sc) = SceneManager.GetActiveScene ();
+//}
