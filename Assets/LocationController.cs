b25e2bd8c9bb6c7a24649fd617428efd9d866183
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-13 04:10:51 +0100

    Added location controller

---
+++ Assets/LocationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocationController : MonoBehaviour
+{
+
+    public string locationString;
+
+
+
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += UpdateLocation;
+    }
+
+    private void UpdateLocation(Scene arg0, LoadSceneMode loadSceneMode)
+    {
+        locationString = SceneManager.GetActiveScene().name;
+    }
+}
