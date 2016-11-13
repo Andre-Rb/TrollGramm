b38bad434a2adf7a45644c2ad37c59bae0bd19cc
maldeirra <malde.clement@hotmail.fr>
2016-11-13 12:06:23 +0100

    ajout de meshes ( serieux je sais meme plus )

---
+++ Assets/ButtonAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonAction : MonoBehaviour
+{
+    public FadeGUI _fadeGui;
+
+
+
+
+    public int firstScene;
+
+    public void LoadFirstLevel()
+    {
+        _fadeGui.BeginFade(1);
+
+        StartCoroutine(FadeOutDone(1));
+
+
+    }
+
+
+
+
+    IEnumerator FadeOutDone(int resultWanted)
+    {
+        yield return new WaitUntil(() => _fadeGui.alpha == resultWanted);
+        SceneManager.LoadSceneAsync(firstScene, LoadSceneMode.Single);
+
+    }
+}
