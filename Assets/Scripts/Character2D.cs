5eaebb68ddd8771b5045e321d4fc7645be234222
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-16 12:55:53 +0100

    moved Tags enums

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -3,116 +3,116 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
-	public float Speed = 8.0f;
-	public float RotationSpeed = 50.0f;
+    public float Speed = 8.0f;
+    public float RotationSpeed = 50.0f;
 
-	public float JumpHeight = 4.0f;
+    public float JumpHeight = 4.0f;
 
-	private bool isGrounded = false;
+    private bool isGrounded = false;
 
-	private float forwardVelocity;
+    private float forwardVelocity;
 
-	private Animator _animator;
-
-
-
-	Rigidbody rb;
-
-	void Start()
-	{
-		rb = GetComponent<Rigidbody>();
-		_animator = GetComponent<Animator>();
-	}
-
-	void Update()
-	{
-		Move();
-		Rotation();
-		Jump();
-	}
-
-	void Move()
-	{
-		float moveH = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-		//float moveV = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-
-		transform.Translate(new Vector3(0, 0, moveH));
-		//rb.AddForce(transform.right * moveH * 4000);
-		//rb.AddForce(transform.forward * moveV * 4000);
-
-		_animator.SetBool(CharacterAnimatorState.isMoving.ToString(), (Math.Abs(0) + Math.Abs(moveH)) > 0);
+    private Animator _animator;
 
 
-		_animator.SetBool(CharacterAnimatorState.isWalkingStraight.ToString(), Input.GetAxis("Vertical") > 0);
-		_animator.SetBool(CharacterAnimatorState.isStraffing.ToString(), Math.Abs(Input.GetAxis("Horizontal")) > 0);
 
-		_animator.SetFloat(CharacterAnimatorState.XWalking.ToString(), Input.GetAxis("Vertical"));
-		_animator.SetFloat(CharacterAnimatorState.YWalking.ToString(), Input.GetAxis("Horizontal"));
-	}
+    Rigidbody rb;
 
-	void Rotation()
-	{
-		float rotH = Input.GetAxis("XboxleftX") * RotationSpeed * Time.deltaTime;
-		float rotV = Input.GetAxis("XboxleftY") * RotationSpeed * Time.deltaTime;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
 
-		if (rotV != 0)
-		{
-			transform.rotation *= Quaternion.Euler(0, rotV, 0);
-			_animator.SetBool(CharacterAnimatorState.isShuffling.ToString(), true);
+    void Update()
+    {
+        Move();
+        Rotation();
+        Jump();
+    }
 
-		}
-		else
-		{
-			_animator.SetBool(CharacterAnimatorState.isShuffling.ToString(), false);
+    void Move()
+    {
+        float moveH = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        //float moveV = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
 
-		}
-	}
+        transform.Translate(new Vector3(0, 0, moveH));
+        //rb.AddForce(transform.right * moveH * 4000);
+        //rb.AddForce(transform.forward * moveV * 4000);
 
-	void Jump()
-	{
-		float jump = Input.GetAxis("Jump") * JumpHeight * Time.deltaTime;
-
-		if (isGrounded)
-		{
-
-			rb.velocity = new Vector3(0, jump * 500, 0);
-
-			_animator.SetTrigger(CharacterAnimatorState.ForwardJump.ToString());
-
-		}
-
-	}
-
-	void OnCollisionStay(Collision other)
-	{
-		if (useGUILayout)
-		{
-
-			isGrounded = true;
-			_animator.SetBool(CharacterAnimatorState.isGrounded.ToString(), true);
-		}
-	}
-
-	void OnCollisionExit(Collision other)
-	{
-		if (isGrounded)
-		{
-			isGrounded = false;
-			_animator.SetBool(CharacterAnimatorState.isGrounded.ToString(), false);
-
-		}
-	}
+        _animator.SetBool(CharacterAnimatorState.isMoving.ToString(), (Math.Abs(0) + Math.Abs(moveH)) > 0);
 
 
-	void CalcForwardVelocity()
-	{
-		Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
-		forwardVelocity = localVelocity.z;
+        _animator.SetBool(CharacterAnimatorState.isWalkingStraight.ToString(), Input.GetAxis("Vertical") > 0);
+        _animator.SetBool(CharacterAnimatorState.isStraffing.ToString(), Math.Abs(Input.GetAxis("Horizontal")) > 0);
 
-		if (forwardVelocity > 0.1f)
-			Debug.Log(forwardVelocity);
-	}
-	/*void OnCollisionEnter(Collider other)
+        _animator.SetFloat(CharacterAnimatorState.XWalking.ToString(), Input.GetAxis("Vertical"));
+        _animator.SetFloat(CharacterAnimatorState.YWalking.ToString(), Input.GetAxis("Horizontal"));
+    }
+
+    void Rotation()
+    {
+        float rotH = Input.GetAxis("XboxleftX") * RotationSpeed * Time.deltaTime;
+        float rotV = Input.GetAxis("XboxleftY") * RotationSpeed * Time.deltaTime;
+
+        if (rotV != 0)
+        {
+            transform.rotation *= Quaternion.Euler(0, rotV, 0);
+            _animator.SetBool(CharacterAnimatorState.isShuffling.ToString(), true);
+
+        }
+        else
+        {
+            _animator.SetBool(CharacterAnimatorState.isShuffling.ToString(), false);
+
+        }
+    }
+
+    void Jump()
+    {
+        float jump = Input.GetAxis("Jump") * JumpHeight * Time.deltaTime;
+
+        if (isGrounded)
+        {
+
+            rb.velocity = new Vector3(0, jump * 500, 0);
+
+            _animator.SetTrigger(CharacterAnimatorState.ForwardJump.ToString());
+
+        }
+
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (useGUILayout)
+        {
+
+            isGrounded = true;
+            _animator.SetBool(CharacterAnimatorState.isGrounded.ToString(), true);
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            _animator.SetBool(CharacterAnimatorState.isGrounded.ToString(), false);
+
+        }
+    }
+
+
+    void CalcForwardVelocity()
+    {
+        Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+        forwardVelocity = localVelocity.z;
+
+        if (forwardVelocity > 0.1f)
+            Debug.Log(forwardVelocity);
+    }
+    /*void OnCollisionEnter(Collider other)
 	{
 		if(other.gameObject.tag == "Pacman")
 		{
@@ -120,24 +120,4 @@ public class Character2D : MonoBehaviour
 			SceneManager.LoadScene ("Game Over");
 		}
 	}*/
-}
-
-
-
-enum CharacterAnimatorState
-{
-
-	XWalking,
-	YWalking,
-	ForwardJump,
-	isGrounded,
-	isShuffling,
-	isWalkingStraight,
-	isStraffing,
-	isMoving
-}
-
-enum Tags
-{
-	Ground
 }
---
+++ Assets/Scripts/StringDB.cs
@@ -0,0 +1,17 @@
+﻿public enum Tags
+{
+    Ground
+}
+
+public enum CharacterAnimatorState
+{
+
+    XWalking,
+    YWalking,
+    ForwardJump,
+    isGrounded,
+    isShuffling,
+    isWalkingStraight,
+    isStraffing,
+    isMoving
+}
