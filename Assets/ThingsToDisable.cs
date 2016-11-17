02e26e7ec2c906115af194cd351bef9601e018d1
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-17 20:50:11 +0100

    New Editor script for faster testing

---
+++ Assets/ThingsToDisable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingsToDisable : MonoBehaviour
+{
+
+
+
+#if UNITY_EDITOR
+    public List<GameObject> List;
+
+    void Start()
+    {
+        DisableAll();
+    }
+    
+    private void DisableAll()
+    {
+        foreach (GameObject o in List)
+        {
+            o.SetActive(false);
+        }
+    }
+
+#endif
+}
---
+++ Assets/ThingsToMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThingsToMove : MonoBehaviour
+{
+    public GameObject GameObject;
+
+
+
+#if UNITY_EDITOR
+    
+    
+
+#endif
+}
