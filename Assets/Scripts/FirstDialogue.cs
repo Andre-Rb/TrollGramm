e7e8942e4af888bf0f7e089c1a256da064ba24c7
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-17 14:31:01 +0100

    Whole new dialogue trigger system with first door working

---
+++ Assets/Scripts/FirstDialogue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FirstDialogue : DialogueTrigger
+{
+    public Animator doorToOpen;
+
+    void OnTriggerEnter()
+    {
+        PlayDialogue();
+
+        GetComponent<Collider>().enabled = false;
+
+    }
+
+    new void DialogueFinishedPlaying()
+    {
+        doorToOpen.SetTrigger(OtherAnimationParams.OpenDoor.ToString());
+
+    }
+
+}
