69b95434d77b6ac1f65c6a41d9e546f45ec81ec6
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-17 15:05:35 +0100

    Death system overhaul

--- Assets/Scripts/SceneManager/Death.cs
+++ Assets/Scripts/SceneManager/Death.cs
@@ -1,33 +1,49 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Death : MonoBehaviour 
 {
+
+    public Transform PlayerTransform;
+
 	public Camera cameraPlayer = null;
 	public Camera cameraGameOver = null;
 	public Text text;
+    public bool PlayerIsDead { get; set; }
 
-	void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player") 
 		{
 			cameraPlayer.gameObject.SetActive (false);
 			cameraGameOver.gameObject.SetActive (true);
 			text.gameObject.SetActive (true);
-		}
+		    PlayerIsDead = true;
+
+
+        }
 	}
 
 	void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Space) && PlayerIsDead)
 		{
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
-			cameraPlayer.gameObject.SetActive (true);
-			cameraGameOver.gameObject.SetActive (false);
-			text.gameObject.SetActive (false);
+			
 
 		}
 	}
+
+
+
+
+    void Respawning()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        cameraPlayer.gameObject.SetActive(true);
+        cameraGameOver.gameObject.SetActive(false);
+        text.gameObject.SetActive(false);
+
+    }
+
 }
