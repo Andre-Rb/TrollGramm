1384bda402c85b398ae5ac8b47fc76a6240f75ff
maldeirra <malde.clement@hotmail.fr>
2016-11-20 16:57:48 +0100

    pacman face  le joueur debut defilement 2d

---
+++ Assets/background.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class background : MonoBehaviour {
+
+	void Update() {
+		// Move the object to the right relative to the camera 1 unit/second.
+		transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+	}
+}
