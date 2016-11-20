850c174acdfec5a131894fe48e4a4d38da2625f8
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-20 15:43:23 +0100

    finished camera select on prefab

--- Assets/CameraSelect.cs
+++ Assets/CameraSelect.cs
@@ -12,15 +12,30 @@ public class CameraSelect : MonoBehaviour
     public bool chooseFPS;
     public bool chooseTPS;
 
+
+    
     void Start()
     {
 
         _playerManetteClavier = GetComponent<Player_Manette_Clavier>();
+        FPSCamera.gameObject.SetActive(false);
+        TPSCamera.gameObject.SetActive(false);
 
         if (chooseFPS)
         {
             _playerManetteClavier.PlayerCamera = FPSCamera;
+            FPSCamera.gameObject.SetActive(true);
 
+        } else if (chooseTPS)
+        {
+            _playerManetteClavier.PlayerCamera = TPSCamera;
+            TPSCamera.gameObject.SetActive(true);
+
+
+        }
+        else
+        {
+            throw new UnityException("You have to choose a camerafor the player");
         }
     }
 
