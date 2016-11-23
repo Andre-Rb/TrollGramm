b94ac99556e0aa1e01dcf2b9cb521b368aaf24c5
Andre-Rb <andre.k.rabe@gmail.com>
2016-11-23 12:17:44 +0100

    reduced scene02scream volume via script

--- Assets/ScreamPacman.cs
+++ Assets/ScreamPacman.cs
@@ -4,19 +4,22 @@ using System.Collections;
 public class ScreamPacman : DialogueTrigger
 {
     public ThirdteenthDialogue Dialogue;
-     Persistance Persistance;
+    Persistance Persistance;
 
     protected override void DialogueFinishedPlaying()
     {
         base.DialogueFinishedPlaying();
+        PlayerAudioSource.volume = 1f;
+
         Dialogue.PlayDialogue();
 
     }
 
 
+    // ReSharper disable once UnusedMember.Local
     void Start()
     {
-
+        PlayerAudioSource.volume = 0.3f;
         Persistance = FindObjectOfType<Persistance>();
 
         if (!Persistance.AlreadyDiedScene02)
